006bee9371f0d2716d7e68f333697bbc7513eb9f
Ashok Shrestha <stha07ashok@gmai.com>
2026-06-23 15:58:17 +0545

    feat: add prometheus metrics endpoint and service-monitor for monitoring

--- server/src/index.ts
+++ server/src/index.ts
@@ -4,6 +4,11 @@ import cors from 'cors';
 import { connectToDatabase } from './connectDB/db.js';
 import todoRoutes from './routes/todoRoutes.js';
 
+import client from 'prom-client';
+
+const collectDefaultMetrics = client.collectDefaultMetrics;
+collectDefaultMetrics();
+
 const app = express();
 app.disable('x-powered-by');
 const PORT = 5000;
@@ -13,6 +18,15 @@ app.use(express.json());
 
 app.get('/', (_req: Request, res: Response) => {
   res.send('Hello, Backend is running successfully!');
+});
+
+app.get('/metrics', async (req, res) => {
+  try {
+    res.set('Content-Type', client.register.contentType);
+    res.end(await client.register.metrics());
+  } catch (err: any) {
+    res.status(500).send(err.message);
+  }
 });
 
 app.use('/api/todos', todoRoutes);
