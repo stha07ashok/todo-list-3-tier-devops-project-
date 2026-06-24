2a984f90f8283c5c95d9cdf906f21875519609d4
Ashok Shrestha <stha07ashok@gmai.com>
2026-06-24 18:13:18 +0545

    creating monitoring environment using grafana cloud

---
+++ client/components/Faro.tsx
@@ -0,0 +1,22 @@
+'use client'; 
+import { useEffect } from 'react';
+import { initializeFaro, getWebInstrumentations } from '@grafana/faro-web-sdk';
+import { TracingInstrumentation } from '@grafana/faro-web-tracing';
+
+export default function Faro() {
+  useEffect(() => {
+    
+    if (typeof window !== 'undefined') {
+      initializeFaro({
+        url: process.env.NEXT_PUBLIC_FARO_URL || '',
+        app: { name: 'todo-list-frontend', version: '1.0.0' },
+        instrumentations: [
+          ...getWebInstrumentations(),
+          new TracingInstrumentation(),
+        ],
+      });
+    }
+  }, []);
+
+  return null;
+}
